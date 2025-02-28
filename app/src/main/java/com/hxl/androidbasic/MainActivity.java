6ec1cf1bb59e5017fe2c34f50582768b83362435
huangxl <413612464@qq.com>
2025-02-28 23:06:05 +0800

    feat: 通过id获取xml元素，编写java修改xml元素内容

--- app/src/main/java/com/hxl/androidbasic/MainActivity.java
+++ app/src/main/java/com/hxl/androidbasic/MainActivity.java
@@ -1,6 +1,7 @@
 package com.hxl.androidbasic;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        TextView tv = findViewById(R.id.tv);
+        tv.setText("你好，学良!");
     }
 }
