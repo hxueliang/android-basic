31e8b4d40ed70bcd76e34403e6f04d4ba32a38fd
huangxl <413612464@qq.com>
2025-02-28 23:34:04 +0800

    feat: Activity跳转

--- app/src/main/java/com/hxl/androidbasic/MainActivity.java
+++ app/src/main/java/com/hxl/androidbasic/MainActivity.java
@@ -1,6 +1,9 @@
 package com.hxl.androidbasic;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -15,5 +18,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         TextView tv = findViewById(R.id.tv);
         tv.setText("你好，学良!");
+
+        Button goActivity2Btn = findViewById(R.id.goActivity2Btn);
+        goActivity2Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
