5551070d99ee478a9402405892a74de52efdda07
huangxl <413612464@qq.com>
2025-03-01 14:34:09 +0800

    feat: textColor属性

--- asimplecontrols/src/main/java/com/hxl/asimplecontrols/TextColorActivity.java
+++ asimplecontrols/src/main/java/com/hxl/asimplecontrols/TextColorActivity.java
@@ -1,6 +1,8 @@
 package com.hxl.asimplecontrols;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,9 @@ public class TextColorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_color);
+
+        TextView tv = findViewById(R.id.tv_text_color);
+        // tv.setTextColor(Color.RED);
+        tv.setTextColor(0xFFFF0000);
     }
 }
