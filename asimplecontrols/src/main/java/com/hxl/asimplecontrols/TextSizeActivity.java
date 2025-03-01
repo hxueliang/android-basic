58030be2963b19a0d73cba6fb8a1aa2b6099fbd2
huangxl <413612464@qq.com>
2025-03-01 13:23:07 +0800

    feat: textSize属性

--- asimplecontrols/src/main/java/com/hxl/asimplecontrols/TextSizeActivity.java
+++ asimplecontrols/src/main/java/com/hxl/asimplecontrols/TextSizeActivity.java
@@ -1,6 +1,7 @@
 package com.hxl.asimplecontrols;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +11,8 @@ public class TextSizeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_size);
+
+        TextView tv = findViewById(R.id.tv_java_set_24sp);
+        tv.setTextSize(24); // 单位sp
     }
 }
