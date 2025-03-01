5dc56d8b59461df3efdbe4d8a9dad59912b4e2af
huangxl <413612464@qq.com>
2025-03-01 10:25:03 +0800

    feat: Activity2跳转Acitvity

--- app/src/main/java/com/hxl/androidbasic/MainActivity2.java
+++ app/src/main/java/com/hxl/androidbasic/MainActivity2.java
@@ -1,6 +1,9 @@
 package com.hxl.androidbasic;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +13,14 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Button goActivity1Btn = findViewById(R.id.goActivity1Btn);
+        goActivity1Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity2.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
