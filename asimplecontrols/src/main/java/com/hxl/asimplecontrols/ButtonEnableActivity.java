57d3846b642cfa8fdfd04433b684e16a79bc30da
huangxl <413612464@qq.com>
2025-03-02 17:33:33 +0800

    feat: 按钮禁用与恢复 enable

--- asimplecontrols/src/main/java/com/hxl/asimplecontrols/ButtonEnableActivity.java
+++ asimplecontrols/src/main/java/com/hxl/asimplecontrols/ButtonEnableActivity.java
@@ -1,11 +1,13 @@
 package com.hxl.asimplecontrols;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ButtonEnableActivity extends AppCompatActivity {
+public class ButtonEnableActivity extends AppCompatActivity implements View.OnClickListener {
+
     private Button btn1;
     private Button btn2;
 
@@ -16,5 +18,12 @@ public class ButtonEnableActivity extends AppCompatActivity {
 
         btn1 = findViewById(R.id.btn1);
         btn2 = findViewById(R.id.btn2);
+
+        btn1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        btn2.setEnabled(true);
     }
 }
