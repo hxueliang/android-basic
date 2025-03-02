c0a084b11918aafd3da62c6d89a629e07de8e818
huangxl <413612464@qq.com>
2025-03-02 12:57:23 +0800

    feat: 长按事件监听 OnLongClickListener

--- asimplecontrols/src/main/java/com/hxl/asimplecontrols/ButtonLongClickListenerActivity.java
+++ asimplecontrols/src/main/java/com/hxl/asimplecontrols/ButtonLongClickListenerActivity.java
@@ -1,6 +1,7 @@
 package com.hxl.asimplecontrols;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -17,5 +18,14 @@ public class ButtonLongClickListenerActivity extends AppCompatActivity {
 
         Button btn = findViewById(R.id.btn);
         tv_msg = findViewById(R.id.tv_msg);
+
+        btn.setOnLongClickListener(new View.OnLongClickListener() {
+
+            @Override
+            public boolean onLongClick(View v) {
+                tv_msg.setText("触发了长按事件");
+                return false; // 自身不消耗事件，让事件继续冒泡
+            }
+        });
     }
 }
