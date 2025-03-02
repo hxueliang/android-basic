15b0e98e45a68528a316b177c674f16c5000bbab
huangxl <413612464@qq.com>
2025-03-02 11:43:50 +0800

    init: 按钮点击事件监听 OnClickListener

---
+++ asimplecontrols/src/main/java/com/hxl/asimplecontrols/ButtonClickListenerActivity.java
@@ -0,0 +1,14 @@
+package com.hxl.asimplecontrols;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ButtonClickListenerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_button_click_listener);
+    }
+}
