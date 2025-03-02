45b6cfc960f6854d7ecb3550db9ac847334d26c6
huangxl <413612464@qq.com>
2025-03-02 11:22:48 +0800

    feat: 按钮 Button onClick事件

--- asimplecontrols/src/main/java/com/hxl/asimplecontrols/ButtonActivity.java
+++ asimplecontrols/src/main/java/com/hxl/asimplecontrols/ButtonActivity.java
@@ -1,14 +1,34 @@
 package com.hxl.asimplecontrols;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.hxl.asimplecontrols.util.DateUtil;
+
 public class ButtonActivity extends AppCompatActivity {
+
+    private TextView tv_message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_button);
+
+        tv_message = findViewById(R.id.tv_message);
+    }
+
+    public void handleClick1(View view) {
+        // view 为 点当前点击的button
+        String dateStr = DateUtil.getNowTime();
+        String message = String.format(
+                "时间：%s，控件：%s",
+                dateStr,
+                ((Button) view).getText() // 将 view 转为 Button 类型，才有 getText 方法
+        );
+        tv_message.setText(message);
     }
 }
