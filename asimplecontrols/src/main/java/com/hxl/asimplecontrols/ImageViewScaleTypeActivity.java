55680f2430fc88006603a1b917f4c8e5795e487f
huangxl <413612464@qq.com>
2025-03-02 18:37:10 +0800

    feat: 图片的缩放类型 scaleType

--- asimplecontrols/src/main/java/com/hxl/asimplecontrols/ImageViewScaleTypeActivity.java
+++ asimplecontrols/src/main/java/com/hxl/asimplecontrols/ImageViewScaleTypeActivity.java
@@ -1,6 +1,7 @@
 package com.hxl.asimplecontrols;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +11,8 @@ public class ImageViewScaleTypeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_view_scale_type);
+
+        ImageView iv_9 = findViewById(R.id.iv_girl_9);
+        iv_9.setScaleType(ImageView.ScaleType.CENTER_CROP);
     }
 }
