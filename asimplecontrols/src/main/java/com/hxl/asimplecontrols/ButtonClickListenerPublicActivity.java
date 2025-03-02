92ecc7e9acf1328f45237b2157127c356e0d1e43
huangxl <413612464@qq.com>
2025-03-02 12:30:55 +0800

    feat: 增加一个按钮测试复用当前类接口

--- asimplecontrols/src/main/java/com/hxl/asimplecontrols/ButtonClickListenerPublicActivity.java
+++ asimplecontrols/src/main/java/com/hxl/asimplecontrols/ButtonClickListenerPublicActivity.java
@@ -19,17 +19,23 @@ public class ButtonClickListenerPublicActivity extends AppCompatActivity impleme
         setContentView(R.layout.activity_button_click_listener_public);
 
         Button btn = findViewById(R.id.btn);
+        Button btn2 = findViewById(R.id.btn2);
         tv_msg = findViewById(R.id.tv_msg);
 
         btn.setOnClickListener(
                 // 传入当前类
                 this
         );
+        btn2.setOnClickListener(this);
     }
 
     // 实现 View.OnClickListener 接口
     @Override
     public void onClick(View v) {
+        if (v.getId() == R.id.btn2) {
+            tv_msg.setText("测试");
+            return;
+        }
         String str = String.format("时间：%s，控件：%s", DateUtil.getNowTime(), ((Button) v).getText());
         tv_msg.setText(str);
     }
