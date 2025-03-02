ee24f9fea710d5b2716d756ca1caede2298fa28a
huangxl <413612464@qq.com>
2025-03-02 17:59:22 +0800

    feat: ImageView控件

--- asimplecontrols/src/main/java/com/hxl/asimplecontrols/ImageViewActivity.java
+++ asimplecontrols/src/main/java/com/hxl/asimplecontrols/ImageViewActivity.java
@@ -1,14 +1,29 @@
 package com.hxl.asimplecontrols;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ImageViewActivity extends AppCompatActivity {
+public class ImageViewActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView iv_girl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_view);
+
+        Button btn = findViewById(R.id.btn);
+        iv_girl = findViewById(R.id.iv_girl);
+
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        iv_girl.setImageResource(R.drawable.ic_boy);
     }
 }
