9f689d3707581ed54ea0cbc93fa4c4e2c09365ed
huangxl <413612464@qq.com>
2025-03-08 17:50:53 +0800

    init: 使用元数据，给应用页面注册快捷方式

---
+++ bactivity/src/main/java/com/hxl/bactivity/JShortcutsActivity.java
@@ -0,0 +1,14 @@
+package com.hxl.bactivity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class JShortcutsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jshortcuts);
+    }
+}
