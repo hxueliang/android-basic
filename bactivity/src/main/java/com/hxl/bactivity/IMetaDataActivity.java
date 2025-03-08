d09f9dbac4a8b71ea871999a7f1dd39fea5e56b3
huangxl <413612464@qq.com>
2025-03-08 16:17:59 +0800

    feat: 获取元数据

--- bactivity/src/main/java/com/hxl/bactivity/IMetaDataActivity.java
+++ bactivity/src/main/java/com/hxl/bactivity/IMetaDataActivity.java
@@ -1,5 +1,7 @@
 package com.hxl.bactivity;
 
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -9,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class IMetaDataActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView tv_meta_data;
+    private PackageManager pm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +20,23 @@ public class IMetaDataActivity extends AppCompatActivity implements View.OnClick
 
         tv_meta_data = findViewById(R.id.tv_meta_data);
         findViewById(R.id.btn_get_meta).setOnClickListener(this);
+
+        // 获取应用包管理器
+        pm = getPackageManager();
     }
 
     @Override
     public void onClick(View v) {
+        try {
+            // 从应用包管理器中获取当前的活动信息
+            ActivityInfo info = pm.getActivityInfo(getComponentName(), PackageManager.GET_META_DATA);
+            // 获取活动附加元数据信息
+            Bundle bundle = info.metaData;
+            String myName = bundle.getString("myName");
+            tv_meta_data.setText(myName);
+
+        } catch (PackageManager.NameNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
