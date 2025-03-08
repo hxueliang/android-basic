afbeeaf98bdd8bfaece04658e5fce2b0e1fba5f7
huangxl <413612464@qq.com>
2025-03-08 19:11:55 +0800

    feat: 形状图形Shape

--- cintermediatecontrols/src/main/java/com/hxl/cintermediatecontrols/ADrawableShapeActivity.java
+++ cintermediatecontrols/src/main/java/com/hxl/cintermediatecontrols/ADrawableShapeActivity.java
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class ADrawableShapeActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private View v_content;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,9 +16,17 @@ public class ADrawableShapeActivity extends AppCompatActivity implements View.On
 
         findViewById(R.id.btn_rect).setOnClickListener(this);
         findViewById(R.id.btn_oval).setOnClickListener(this);
+
+        v_content = findViewById(R.id.v_content);
+        v_content.setBackgroundResource(R.drawable.shape_rect_gold);
     }
 
     @Override
     public void onClick(View v) {
+        if (v.getId() == R.id.btn_rect) {
+            v_content.setBackgroundResource(R.drawable.shape_rect_gold);
+        } else {
+            v_content.setBackgroundResource(R.drawable.shape_oval_rose);
+        }
     }
 }
