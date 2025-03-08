ecaf2f7a7d2f54803f9b96f1814f44853972461b
huangxl <413612464@qq.com>
2025-03-08 11:48:04 +0800

    feat: 隐式Intent

--- bactivity/src/main/java/com/hxl/bactivity/EIntentActivity.java
+++ bactivity/src/main/java/com/hxl/bactivity/EIntentActivity.java
@@ -1,6 +1,7 @@
 package com.hxl.bactivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
@@ -21,11 +22,39 @@ public class EIntentActivity extends AppCompatActivity implements View.OnClickLi
     @Override
     public void onClick(View v) {
         Intent intent = new Intent();
+        String phoneNo = "1383838";
         if (v.getId() == R.id.btn_dial) {
+            // 设置意图动作为：拨号
+            intent.setAction(Intent.ACTION_DIAL);
+            // 声明一个拨号Uri
+            Uri uri = Uri.parse("tel:" + phoneNo);
+            intent.setData(uri);
 
         } else if (v.getId() == R.id.btn_sms) {
+            // 设置意图动作为：发短信
+            intent.setAction(Intent.ACTION_SENDTO);
+            // 声明一个短信Uri
+            Uri uri = Uri.parse("smsto:" + phoneNo);
+            intent.setData(uri);
 
         } else if (v.getId() == R.id.btn_my) {
+            // 设置意图动作为：android.intent.action.HXL
+            intent.setAction("android.intent.action.HXL");
+            // 设置category
+            intent.addCategory(Intent.CATEGORY_DEFAULT);
+
+            // 与当前手机以安装的app中，匹配其清单文件中<intent-filter>
+            /**
+             * <intent-filter>
+             *     <action android:name="android.intent.action.MAIN" />
+             *
+             *     <category android:name="android.intent.category.LAUNCHER" />
+             * </intent-filter>
+             */
+
+            // 并且当前手机以安装的app中，匹配其清单文件中activity中的exported为true：表示请允许其它应用打开
+            /**
+             * <activity android:exported="true" >
 
         }
         startActivity(intent);
