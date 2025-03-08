46d4efa74bf11f0922c0e50e8087f7949bbe2f0d
huangxl <413612464@qq.com>
2025-03-08 19:11:44 +0800

    init: 形状图形Shape

--- cintermediatecontrols/src/main/java/com/hxl/cintermediatecontrols/ADrawableShapeActivity.java
+++ cintermediatecontrols/src/main/java/com/hxl/cintermediatecontrols/ADrawableShapeActivity.java
@@ -1,14 +1,22 @@
 package com.hxl.cintermediatecontrols;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ADrawableShapeActivity extends AppCompatActivity {
+public class ADrawableShapeActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adrawable_shape);
+
+        findViewById(R.id.btn_rect).setOnClickListener(this);
+        findViewById(R.id.btn_oval).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
     }
 }
