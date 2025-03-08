662b9eb4e555bc7c8f578ff93d35a1a4cc24b530
huangxl <413612464@qq.com>
2025-03-08 18:44:46 +0800

    init: 创建cIntermediateControls模块

---
+++ cintermediatecontrols/src/main/java/com/hxl/cintermediatecontrols/ADrawableShapeActivity.java
@@ -0,0 +1,14 @@
+package com.hxl.cintermediatecontrols;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ADrawableShapeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adrawable_shape);
+    }
+}
