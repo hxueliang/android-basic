12d7466e53d774763d3e75631932fe54eab218dc
huangxl <413612464@qq.com>
2025-03-09 18:04:31 +0800

    feat: 本文输入框EditText-焦点变更监听器

--- cintermediatecontrols/src/main/java/com/hxl/cintermediatecontrols/IEditTextFocusActivity.java
+++ cintermediatecontrols/src/main/java/com/hxl/cintermediatecontrols/IEditTextFocusActivity.java
@@ -1,11 +1,13 @@
 package com.hxl.cintermediatecontrols;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class IEditTextFocusActivity extends AppCompatActivity {
+public class IEditTextFocusActivity extends AppCompatActivity implements View.OnFocusChangeListener {
 
     private EditText et_phone;
     private EditText et_pwd;
@@ -17,5 +19,19 @@ public class IEditTextFocusActivity extends AppCompatActivity {
 
         et_phone = findViewById(R.id.et_phone);
         et_pwd = findViewById(R.id.et_pwd);
+
+        et_pwd.setOnFocusChangeListener(this);
+    }
+
+    @Override
+    public void onFocusChange(View v, boolean hasFocus) {
+        if (hasFocus) {
+            String phone = et_phone.getText().toString();
+
+            if (phone.isEmpty() || phone.length() < 11) {
+                et_phone.requestFocus();
+                Toast.makeText(this, "请输入11位手机号码", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
