8157cba35ed2a6135e5d6cfbcfd8377f78af6020
huangxl <413612464@qq.com>
2025-03-09 12:23:24 +0800

    feat: 设置复选框文案

--- cintermediatecontrols/src/main/java/com/hxl/cintermediatecontrols/DCheckBoxActivity.java
+++ cintermediatecontrols/src/main/java/com/hxl/cintermediatecontrols/DCheckBoxActivity.java
@@ -1,14 +1,28 @@
 package com.hxl.cintermediatecontrols;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class DCheckBoxActivity extends AppCompatActivity {
+public class DCheckBoxActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dcheck_box);
+
+        CheckBox ck_system = findViewById(R.id.ck_system);
+        CheckBox ck_custom = findViewById(R.id.ck_custom);
+
+        ck_system.setOnCheckedChangeListener(this);
+        ck_custom.setOnCheckedChangeListener(this);
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        String str = String.format("%s了复选框", isChecked ? "勾选" : "取消勾选");
+        buttonView.setText(str);
     }
 }
