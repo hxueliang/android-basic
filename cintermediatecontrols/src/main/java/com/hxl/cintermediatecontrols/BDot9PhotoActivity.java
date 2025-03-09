e3bbbc04f3ad451bcd20b447c023b88eca05b5ff
huangxl <413612464@qq.com>
2025-03-09 09:49:34 +0800

    init: 使用.9图片

---
+++ cintermediatecontrols/src/main/java/com/hxl/cintermediatecontrols/BDot9PhotoActivity.java
@@ -0,0 +1,14 @@
+package com.hxl.cintermediatecontrols;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BDot9PhotoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bdot9_photo);
+    }
+}
