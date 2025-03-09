5761e9499c305522dcc00a4e6157025bdcdd3a66
huangxl <413612464@qq.com>
2025-03-09 17:24:26 +0800

    init: 本文输入框EditText-自定义样式

---
+++ cintermediatecontrols/src/main/java/com/hxl/cintermediatecontrols/HEditTextStyleActivity.java
@@ -0,0 +1,14 @@
+package com.hxl.cintermediatecontrols;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HEditTextStyleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hedit_text_style);
+    }
+}
