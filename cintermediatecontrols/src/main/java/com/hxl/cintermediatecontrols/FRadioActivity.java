04e6dde0fd3228bb398602ae1a6a2e8f22a6bf8e
huangxl <413612464@qq.com>
2025-03-09 15:47:55 +0800

    feat: 单选框Radio

--- cintermediatecontrols/src/main/java/com/hxl/cintermediatecontrols/FRadioActivity.java
+++ cintermediatecontrols/src/main/java/com/hxl/cintermediatecontrols/FRadioActivity.java
@@ -1,11 +1,12 @@
 package com.hxl.cintermediatecontrols;
 
 import android.os.Bundle;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class FRadioActivity extends AppCompatActivity {
+public class FRadioActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener {
 
     private TextView tv_result;
 
@@ -15,6 +16,20 @@ public class FRadioActivity extends AppCompatActivity {
         setContentView(R.layout.activity_fradio);
 
         tv_result = findViewById(R.id.tv_result);
+        RadioGroup rg_gender = findViewById(R.id.rg_gender);
 
+        rg_gender.setOnCheckedChangeListener(this);
+
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        if (checkedId == R.id.rb_male) {
+            String desc = String.format("您的性别是：%s", "男");
+            tv_result.setText(desc);
+        } else if (checkedId == R.id.rb_female) {
+            String desc = String.format("您的性别是：%s", "女");
+            tv_result.setText(desc);
+        }
     }
 }
