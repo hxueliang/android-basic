76196567842df57a5c50bdacb814bc15f0e32702
huangxl <413612464@qq.com>
2025-03-10 09:27:55 +0800

    init: 日期选择器DatePicker

---
+++ cintermediatecontrols/src/main/java/com/hxl/cintermediatecontrols/LDatePickerActivity.java
@@ -0,0 +1,26 @@
+package com.hxl.cintermediatecontrols;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LDatePickerActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView tv_message;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ldate_picker);
+
+        tv_message = findViewById(R.id.tv_message);
+        findViewById(R.id.btn_confirm).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
