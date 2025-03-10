032952949692d0a7c68bb8b48cac3524b9b8faff
huangxl <413612464@qq.com>
2025-03-10 19:12:47 +0800

    feat: 时间选择器TimePicker

--- cintermediatecontrols/src/main/java/com/hxl/cintermediatecontrols/NTimePickerActivity.java
+++ cintermediatecontrols/src/main/java/com/hxl/cintermediatecontrols/NTimePickerActivity.java
@@ -3,12 +3,14 @@ package com.hxl.cintermediatecontrols;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class NTimePickerActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView tv_message;
+    private TimePicker tp_time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +18,14 @@ public class NTimePickerActivity extends AppCompatActivity implements View.OnCli
         setContentView(R.layout.activity_ntime_picker);
 
         tv_message = findViewById(R.id.tv_message);
+        tp_time = findViewById(R.id.tp_time);
+        tp_time.setIs24HourView(true);
         findViewById(R.id.btn_confirm).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
+        String desc = String.format("%s时%s分", tp_time.getHour(), tp_time.getMinute());
+        tv_message.setText(desc);
     }
 }
