0efa2063ebb0bb265254ab7eeaa4c5633cfaf710
huangxl <413612464@qq.com>
2025-03-10 09:28:08 +0800

    feat: 日期选择器DatePicker

--- cintermediatecontrols/src/main/java/com/hxl/cintermediatecontrols/LDatePickerActivity.java
+++ cintermediatecontrols/src/main/java/com/hxl/cintermediatecontrols/LDatePickerActivity.java
@@ -2,6 +2,7 @@ package com.hxl.cintermediatecontrols;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class LDatePickerActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView tv_message;
+    private DatePicker dp_date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +18,13 @@ public class LDatePickerActivity extends AppCompatActivity implements View.OnCli
         setContentView(R.layout.activity_ldate_picker);
 
         tv_message = findViewById(R.id.tv_message);
+        dp_date = findViewById(R.id.dp_date);
         findViewById(R.id.btn_confirm).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-
+        String desc = String.format("%s年%s月%s日", dp_date.getYear(), dp_date.getMonth() + 1, dp_date.getDayOfMonth());
+        tv_message.setText(desc);
     }
 }
