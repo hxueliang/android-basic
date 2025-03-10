2cb5ebb0f33b38b6f5e10236cfbbeca7046140a0
huangxl <413612464@qq.com>
2025-03-10 09:40:35 +0800

    feat: 日期弹窗选择器DatePickerDialog

--- cintermediatecontrols/src/main/java/com/hxl/cintermediatecontrols/MDatePickerDialogActivity.java
+++ cintermediatecontrols/src/main/java/com/hxl/cintermediatecontrols/MDatePickerDialogActivity.java
@@ -1,12 +1,14 @@
 package com.hxl.cintermediatecontrols;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MDatePickerDialogActivity extends AppCompatActivity implements View.OnClickListener {
+public class MDatePickerDialogActivity extends AppCompatActivity implements View.OnClickListener, DatePickerDialog.OnDateSetListener {
 
     private TextView tv_message;
 
@@ -21,6 +23,13 @@ public class MDatePickerDialogActivity extends AppCompatActivity implements View
 
     @Override
     public void onClick(View v) {
+        DatePickerDialog dialog = new DatePickerDialog(this, this, 2025, 3 + 1, 10);
+        dialog.show();
+    }
 
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        String desc = String.format("%s年%s月%s日", year, month + 1, dayOfMonth);
+        tv_message.setText(desc);
     }
 }
