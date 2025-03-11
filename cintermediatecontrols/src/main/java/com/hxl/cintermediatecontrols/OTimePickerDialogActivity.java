4bc839be4b53ff44acd8e2f987abfcfbd3460007
huangxl <413612464@qq.com>
2025-03-11 09:18:24 +0800

    feat: 时间弹窗选择器TimePickerDialog

--- cintermediatecontrols/src/main/java/com/hxl/cintermediatecontrols/OTimePickerDialogActivity.java
+++ cintermediatecontrols/src/main/java/com/hxl/cintermediatecontrols/OTimePickerDialogActivity.java
@@ -1,12 +1,14 @@
 package com.hxl.cintermediatecontrols;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class OTimePickerDialogActivity extends AppCompatActivity implements View.OnClickListener {
+public class OTimePickerDialogActivity extends AppCompatActivity implements View.OnClickListener, TimePickerDialog.OnTimeSetListener {
 
     private TextView tv_message;
 
@@ -21,6 +23,20 @@ public class OTimePickerDialogActivity extends AppCompatActivity implements View
 
     @Override
     public void onClick(View v) {
+        TimePickerDialog dialog = new TimePickerDialog(
+                this,
+                android.R.style.Theme_Holo_Light_Dialog,
+                this,
+                12,
+                15,
+                true
+        );
+        dialog.show();
+    }
 
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        String desc = String.format("%s时%s分", hourOfDay, minute);
+        tv_message.setText(desc);
     }
 }
