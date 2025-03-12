0b82f71127af3d05698227883a0f18ade23a6c58
huangxl <413612464@qq.com>
2025-03-12 09:02:03 +0800

    init: 数据持久化SharedPreferences

---
+++ dlocaldatalasting/src/main/java/com/hxl/dlocaldatalasting/BSharedPreferencesActivity.java
@@ -0,0 +1,38 @@
+package com.hxl.dlocaldatalasting;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BSharedPreferencesActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText et_name;
+    private EditText et_age;
+    private EditText et_height;
+    private CheckBox ck_married;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bshare_references);
+
+        et_name = findViewById(R.id.et_name);
+        et_age = findViewById(R.id.et_age);
+        et_height = findViewById(R.id.et_height);
+        ck_married = findViewById(R.id.ck_married);
+        findViewById(R.id.btn_save).setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        String name = et_name.getText().toString();
+        String age = et_age.getText().toString();
+        String height = et_height.getText().toString();
+        Boolean married = ck_married.isChecked();
+
+    }
+}
