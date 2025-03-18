1fb74653affc4280bfad912be2808311a5a9edd9
huangxl <413612464@qq.com>
2025-03-18 10:29:00 +0800

    feat: 使用Application存储数据-适合在Application中保存的全局变量

--- dlocaldatalasting/src/main/java/com/hxl/dlocaldatalasting/HMyApplication.java
+++ dlocaldatalasting/src/main/java/com/hxl/dlocaldatalasting/HMyApplication.java
@@ -6,6 +6,13 @@ import java.util.HashMap;
 
 public class HMyApplication extends Application {
 
+    /**
+     * 适合在Application中保存的全局变量主要有3类数据：
+     * 1. 会频繁读取的信息，如用户名、手机号等
+     * 2. 不方便由意图传递的数据，如位图对象、非字符串型的集合对象等
+     * 3. 容易因频繁分配内存而导致内存泄漏的对象，如Handler对象等
+     */
+
     // 声明一个变量，用于储存当前类实例
     private static HMyApplication mApp;
 
