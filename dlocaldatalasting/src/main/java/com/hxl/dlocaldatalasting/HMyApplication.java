ca9701eb845be8b107be6d9cdc3e84d77c945fa4
huangxl <413612464@qq.com>
2025-03-18 09:50:32 +0800

    init: 使用Application存储数据

---
+++ dlocaldatalasting/src/main/java/com/hxl/dlocaldatalasting/HMyApplication.java
@@ -0,0 +1,10 @@
+package com.hxl.dlocaldatalasting;
+
+import android.app.Application;
+
+public class HMyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
