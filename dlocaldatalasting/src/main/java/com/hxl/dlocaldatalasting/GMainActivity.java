a9f9fdd3359dfa76d68655003b1d7bbc9f2275c8
huangxl <413612464@qq.com>
2025-03-18 09:28:28 +0800

    init: Application的生命周期

---
+++ dlocaldatalasting/src/main/java/com/hxl/dlocaldatalasting/GMainActivity.java
@@ -0,0 +1,14 @@
+package com.hxl.dlocaldatalasting;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gmain);
+    }
+}
