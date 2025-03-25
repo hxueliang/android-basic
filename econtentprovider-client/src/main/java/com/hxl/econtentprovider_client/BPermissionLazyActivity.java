a3731af8b8d3800fe84c6967f622c4e01fa9b5ec
huangxl <413612464@qq.com>
2025-03-25 09:58:19 +0800

    feat: 内容提供者ContentProvider-运行时动态申请权限Lazy模式

--- econtentprovider-client/src/main/java/com/hxl/econtentprovider_client/BPermissionLazyActivity.java
+++ econtentprovider-client/src/main/java/com/hxl/econtentprovider_client/BPermissionLazyActivity.java
@@ -1,11 +1,35 @@
 package com.hxl.econtentprovider_client;
 
+import android.Manifest;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.hxl.econtentprovider_client.util.PermissionUtil;
+import com.hxl.econtentprovider_client.util.ToastUtil;
+
 public class BPermissionLazyActivity extends AppCompatActivity implements View.OnClickListener {
+
+    // 通过string数据定义，需要通讯录的读写权限
+    private static final String[] PERMISSIONS_CONTACTS = new String[]{
+            Manifest.permission.READ_CONTACTS,
+            Manifest.permission.WRITE_CONTACTS
+    };
+
+    // 短信读写权限
+    private static final String[] PERMISSIONS_SMS = new String[]{
+            Manifest.permission.SEND_SMS,
+            Manifest.permission.READ_SMS
+    };
+
+    private static final int REQUEST_CODE_CONTACTS = 1;
+    private static final int REQUEST_CODE_SMS = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +43,38 @@ public class BPermissionLazyActivity extends AppCompatActivity implements View.O
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.btn_contact) {
+            PermissionUtil.checkPermission(this, PERMISSIONS_CONTACTS, REQUEST_CODE_CONTACTS);
         } else if (v.getId() == R.id.btn_sms) {
+            PermissionUtil.checkPermission(this, PERMISSIONS_SMS, REQUEST_CODE_SMS);
         }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE_CONTACTS) {
+            if (PermissionUtil.checkGrant(grantResults)) {
+                Log.d("x_log", "通讯录权限获取成功");
+            } else {
+                ToastUtil.show(this, "获取通讯录读写权限失败");
+                jumpToSettings();
+            }
+        } else if (requestCode == REQUEST_CODE_SMS) {
+            if (PermissionUtil.checkGrant(grantResults)) {
+                Log.d("x_log", "收发短信权限获取成功");
+            } else {
+                ToastUtil.show(this, "获取收发短信权限失败");
+                jumpToSettings();
+            }
+        }
+    }
+
+    // 跳转到应用设置界面
+    private void jumpToSettings() {
+        Intent intent = new Intent();
+        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        intent.setData(Uri.fromParts("package", getPackageName(), null));
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
