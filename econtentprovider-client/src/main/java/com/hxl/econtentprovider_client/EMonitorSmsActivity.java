ca2ce7b7f889f1b089e462cdbd86676bf755e3f1
huangxl <413612464@qq.com>
2025-03-28 10:27:03 +0800

    init: 短信观察器ContentObserver

---
+++ econtentprovider-client/src/main/java/com/hxl/econtentprovider_client/EMonitorSmsActivity.java
@@ -0,0 +1,14 @@
+package com.hxl.econtentprovider_client;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EMonitorSmsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emonitor_sms);
+    }
+}
