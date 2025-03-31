f56868da10014995b50d2d772fa437a278faf23c
huangxl <413612464@qq.com>
2025-03-31 19:40:04 +0800

    feat: 发送彩信-通过MediaStore查询图片

--- econtentprovider-client/src/main/java/com/hxl/econtentprovider_client/GMediaStoreImageActivity.java
+++ econtentprovider-client/src/main/java/com/hxl/econtentprovider_client/GMediaStoreImageActivity.java
@@ -1,12 +1,22 @@
 package com.hxl.econtentprovider_client;
 
+import android.annotation.SuppressLint;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.hxl.econtentprovider_client.entity.ImageInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class GMediaStoreImageActivity extends AppCompatActivity {
 
+    private final List<ImageInfo> mImageList = new ArrayList<>();
     private TextView et_phone;
     private TextView et_title;
     private TextView et_content;
@@ -19,5 +29,39 @@ public class GMediaStoreImageActivity extends AppCompatActivity {
         et_phone = findViewById(R.id.et_phone);
         et_title = findViewById(R.id.et_title);
         et_content = findViewById(R.id.et_content);
+
+        loadImageList();
+    }
+
+    // 加载图片列表
+    @SuppressLint("Range")
+    private void loadImageList() {
+        String[] columns = {
+                MediaStore.Images.Media._ID, // 编号
+                MediaStore.Images.Media.TITLE, // 标题（文件名）
+                MediaStore.Images.Media.SIZE, // 文件大小
+                MediaStore.Images.Media.DATA, // 文件路径
+        };
+        Cursor cursor = getContentResolver().query(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                columns,
+                "_size < 300 * 1024", // 300kb
+                null,
+                "_size DESC"
+        );
+        if (cursor == null) {
+            return;
+        }
+        int count = 0;
+        while (cursor.moveToNext() && count < 6) {
+            ImageInfo imageInfo = new ImageInfo();
+            imageInfo.id = cursor.getLong(cursor.getColumnIndex(MediaStore.Images.Media._ID));
+            imageInfo.name = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.TITLE));
+            imageInfo.size = cursor.getLong(cursor.getColumnIndex(MediaStore.Images.Media.SIZE));
+            imageInfo.path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+            count++;
+            mImageList.add(imageInfo);
+            Log.d("x_log", "mImageList = " + mImageList);
+        }
     }
 }
---
+++ econtentprovider-client/src/main/java/com/hxl/econtentprovider_client/entity/ImageInfo.java
@@ -0,0 +1,19 @@
+package com.hxl.econtentprovider_client.entity;
+
+public class ImageInfo {
+    public long id;
+    public String name;
+    public long size;
+    public String path;
+
+    @Override
+    public String toString() {
+        return "ImageInfo{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", size=" + size +
+                ", path='" + path + '\'' +
+                '}';
+    }
+}
+
