1fa469ba28a43db2311c3133771da0c3ca636b34
huangxl <413612464@qq.com>
2025-04-01 13:36:29 +0800

    feat: 发送彩信-通过MediaStore查询图片并展示在页面

--- econtentprovider-client/src/main/java/com/hxl/econtentprovider_client/GMediaStoreImageActivity.java
+++ econtentprovider-client/src/main/java/com/hxl/econtentprovider_client/GMediaStoreImageActivity.java
@@ -1,25 +1,45 @@
 package com.hxl.econtentprovider_client;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaScannerConnection;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.GridLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.hxl.econtentprovider_client.entity.ImageInfo;
+import com.hxl.econtentprovider_client.util.PermissionUtil;
+import com.hxl.econtentprovider_client.util.Utils;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class GMediaStoreImageActivity extends AppCompatActivity {
 
+    // 通过string数据定义，需要的权限
+    private static final String[] PERMISSIONS = new String[]{
+            // Manifest.permission.READ_EXTERNAL_STORAGE, // api13以下使用
+            Manifest.permission.READ_MEDIA_IMAGES // api13以上使用
+    };
+
+    private static final int PERMISSION_REQUEST_CODE = 1;
+
     private final List<ImageInfo> mImageList = new ArrayList<>();
     private TextView et_phone;
     private TextView et_title;
     private TextView et_content;
+    private GridLayout gl_appendix;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +50,63 @@ public class GMediaStoreImageActivity extends AppCompatActivity {
         et_title = findViewById(R.id.et_title);
         et_content = findViewById(R.id.et_content);
 
-        loadImageList();
+        gl_appendix = findViewById(R.id.gl_appendix);
+
+        // 手动让MediaStore扫描入库
+        MediaScannerConnection.scanFile(
+                this,
+                new String[]{Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString()},
+                null,
+                null
+        );
+
+        if (PermissionUtil.checkPermission(this, PERMISSIONS, PERMISSION_REQUEST_CODE)) {
+            loadImageList();
+
+            showImageGrid();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (
+                requestCode == PERMISSION_REQUEST_CODE &&
+                        PermissionUtil.checkGrant(grantResults)
+        ) {
+
+            loadImageList();
+
+            showImageGrid();
+
+        }
+    }
+
+    // 显示图像网格
+    private void showImageGrid() {
+        gl_appendix.removeAllViews();
+
+        for (ImageInfo image : mImageList) {
+            // image -> ImageView
+            ImageView iv_appendix = new ImageView(this);
+            Bitmap bitmap = BitmapFactory.decodeFile(image.path);
+            iv_appendix.setImageBitmap(bitmap);
+            // 设置图像视图的缩放类型
+            iv_appendix.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            // 设置图像视图的布局参数
+            int px = Utils.dip2px(this, 110);
+            ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(px, px);
+            iv_appendix.setLayoutParams(params);
+            // 设置图像视图的内部间距
+            int padding = Utils.dip2px(this, 5);
+            iv_appendix.setPadding(padding, padding, padding, padding);
+            iv_appendix.setOnClickListener(v -> {
+
+            });
+            // 把图像添加到网格布局
+            Log.d("x_log", iv_appendix.toString());
+            gl_appendix.addView(iv_appendix);
+        }
     }
 
     // 加载图片列表
---
+++ econtentprovider-client/src/main/java/com/hxl/econtentprovider_client/util/Utils.java
@@ -0,0 +1,15 @@
+package com.hxl.econtentprovider_client.util;
+
+import android.content.Context;
+
+public class Utils {
+
+    // 根据手机的分辨率把 dp 的单位转成为 px
+    public static int dip2px(Context context, float dipValue) {
+        // 获取当前手机的像素密度（1个dp对应几个px）
+        float scale = context.getResources().getDisplayMetrics().density;
+        // 四舍五入
+        return (int) (dipValue * scale + 0.5f);
+    }
+
+}
