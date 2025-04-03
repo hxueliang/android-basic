8706d5a168d02eea007731e4dba60980fd9fe23f
huangxl <413612464@qq.com>
2025-04-03 09:56:05 +0800

    feat: 下拉列表Spinner

--- fadvancedcontrols/src/main/java/com/hxl/fadvancedcontrols/BSpinnerDropdownActivity.java
+++ fadvancedcontrols/src/main/java/com/hxl/fadvancedcontrols/BSpinnerDropdownActivity.java
@@ -1,14 +1,43 @@
 package com.hxl.fadvancedcontrols;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class BSpinnerDropdownActivity extends AppCompatActivity {
+import com.hxl.fadvancedcontrols.util.ToastUtil;
+
+public class BSpinnerDropdownActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private final static String[] startArray = {"金星", "木星", "水星", "火星", "土星"};
+    private Spinner sp_dropdown;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bspinner_dropdown);
+
+        sp_dropdown = findViewById(R.id.sp_dropdown);
+
+        // 声明一个下拉列表的数组适配器
+        ArrayAdapter<String> startAdapter = new ArrayAdapter<>(this, R.layout.b_item_select, startArray);
+        sp_dropdown.setAdapter(startAdapter);
+        // 设置下拉框默认显示第一项
+        sp_dropdown.setSelection(0);
+        // 给下拉框设置选择监听器，一旦用户选中某一项，就触发监听器的 onItemSelected 方法
+        sp_dropdown.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        ToastUtil.show(this, "您选择的是：" + startArray);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
