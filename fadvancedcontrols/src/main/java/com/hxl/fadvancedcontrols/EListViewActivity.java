d08c88c82a854a838a9613d88f9e3e2be8a08719
huangxl <413612464@qq.com>
2025-04-14 09:10:16 +0800

    feat: 列表视图ListView

--- fadvancedcontrols/src/main/java/com/hxl/fadvancedcontrols/EListViewActivity.java
+++ fadvancedcontrols/src/main/java/com/hxl/fadvancedcontrols/EListViewActivity.java
@@ -1,11 +1,21 @@
 package com.hxl.fadvancedcontrols;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class EListViewActivity extends AppCompatActivity {
+import com.hxl.fadvancedcontrols.adapter.DPlanetBaseAdapter;
+import com.hxl.fadvancedcontrols.bean.DPlanet;
+import com.hxl.fadvancedcontrols.util.ToastUtil;
+
+import java.util.List;
+
+public class EListViewActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    private List<DPlanet> planetList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +23,15 @@ public class EListViewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_elist_view);
 
         final ListView lv_planet = findViewById(R.id.lv_planet);
+        planetList = DPlanet.getDefaultList();
+        DPlanetBaseAdapter adapter = new DPlanetBaseAdapter(this, planetList);
+        lv_planet.setAdapter(adapter);
+
+        lv_planet.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        ToastUtil.show(this, "你选择的是：" + planetList.get(position).name);
     }
 }
