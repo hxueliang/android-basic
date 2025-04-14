68f28795f2380d612836d6c3ae2bc0f0103c83a3
huangxl <413612464@qq.com>
2025-04-14 12:57:50 +0800

    init: ListView条目事件冲突

---
+++ fadvancedcontrols/src/main/java/com/hxl/fadvancedcontrols/FListFocusActivity.java
@@ -0,0 +1,14 @@
+package com.hxl.fadvancedcontrols;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FListFocusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flist_focus);
+    }
+}
