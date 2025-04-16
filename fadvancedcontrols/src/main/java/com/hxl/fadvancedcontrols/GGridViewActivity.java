22d4398a2ecc503bc2be2ce9a93b9f8548f986a5
huangxl <413612464@qq.com>
2025-04-16 08:43:49 +0800

    feat: 网络视图GridView-数据渲染和事件绑定

--- fadvancedcontrols/src/main/java/com/hxl/fadvancedcontrols/GGridViewActivity.java
+++ fadvancedcontrols/src/main/java/com/hxl/fadvancedcontrols/GGridViewActivity.java
@@ -1,14 +1,38 @@
 package com.hxl.fadvancedcontrols;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class GGridViewActivity extends AppCompatActivity {
+import com.hxl.fadvancedcontrols.adapter.GPlanetGridAdapter;
+import com.hxl.fadvancedcontrols.bean.DPlanet;
+import com.hxl.fadvancedcontrols.util.ToastUtil;
+
+import java.util.List;
+
+public class GGridViewActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    private GridView gv_planet;
+    private List<DPlanet> dPlanetList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ggrid_view);
+
+        gv_planet = findViewById(R.id.gv_planet);
+        dPlanetList = DPlanet.getDefaultList();
+        final GPlanetGridAdapter gPlanetGridAdapter = new GPlanetGridAdapter(this, dPlanetList);
+        gv_planet.setAdapter(gPlanetGridAdapter);
+
+        gv_planet.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        ToastUtil.show(this, "选择的是：" + dPlanetList.get(position).name);
     }
 }
---
+++ fadvancedcontrols/src/main/java/com/hxl/fadvancedcontrols/adapter/GPlanetGridAdapter.java
@@ -0,0 +1,70 @@
+package com.hxl.fadvancedcontrols.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.hxl.fadvancedcontrols.R;
+import com.hxl.fadvancedcontrols.bean.DPlanet;
+
+import java.util.List;
+
+public class GPlanetGridAdapter extends BaseAdapter {
+    private final Context mContext;
+    private final List<DPlanet> mPlaneList;
+
+    // 右键 -> Generate -> Constructor
+    public GPlanetGridAdapter(Context mContext, List<DPlanet> mPlaneList) {
+        this.mContext = mContext;
+        this.mPlaneList = mPlaneList;
+    }
+
+    @Override
+    public int getCount() {
+        return mPlaneList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mPlaneList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView == null) {
+            // 根据布局文件item_list.xml生成转换视图对象
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.g_item_grid, null);
+            holder = new ViewHolder();
+            holder.iv_icon = convertView.findViewById(R.id.iv_icon);
+            holder.tv_name = convertView.findViewById(R.id.tv_name);
+            holder.tv_desc = convertView.findViewById(R.id.tv_desc);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        // 控件设置数据
+        final DPlanet dPlanet = mPlaneList.get(position);
+        holder.iv_icon.setImageResource(dPlanet.image);
+        holder.tv_name.setText(dPlanet.name);
+        holder.tv_desc.setText(dPlanet.desc);
+
+        return convertView;
+    }
+
+    public final class ViewHolder {
+        public ImageView iv_icon;
+        public TextView tv_name;
+        public TextView tv_desc;
+    }
+}
