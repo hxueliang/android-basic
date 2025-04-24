ebfcd12a0d247a002dcbfa50afa8c9fc8152f1f8
huangxl <413612464@qq.com>
2025-04-24 13:44:18 +0800

    feat: 实现Runnable和继承Thread区别

--- gmmultithreading/src/main/java/com/hxl/gmmultithreading/AExtendsThreadActivity.java
+++ gmmultithreading/src/main/java/com/hxl/gmmultithreading/AExtendsThreadActivity.java
@@ -6,6 +6,17 @@ import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AExtendsThreadActivity extends AppCompatActivity {
+    /**
+     * 实现Runnable和继承Thread区别
+     * 1. 一个类只能继承一个父类，存在局限；一个类可以实现多个接口；
+     * 2. 在实现Runnable接口的时候用Thread(Runnable target)创建进程时，
+     * 使用同一个Runnable实例，则建立的建立的多线程的实例变量也是共享的。
+     * 但是通过继承Thread类是不能用一个实例建立多个线程， 故而实现Runnable接口适合于资源共享。
+     * 当然，继承Thread类也能够共享变量，能共享Thread类的static变量(通常不会这么做)；
+     * 3. Runnable接口和Thread之间的联系：
+     * public class Thread extends Object implements Runnable
+     * 可以看出Thread类也是Runnable接口的子类
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
