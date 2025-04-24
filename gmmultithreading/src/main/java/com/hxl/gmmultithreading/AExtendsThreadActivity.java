1cf6631d511b7784cffca805b9101d2338f9e19f
huangxl <413612464@qq.com>
2025-04-24 09:14:59 +0800

    init: 通过继承Thread类，实现多线程

---
+++ gmmultithreading/src/main/java/com/hxl/gmmultithreading/AExtendsThreadActivity.java
@@ -0,0 +1,18 @@
+package com.hxl.gmmultithreading;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AExtendsThreadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aextends_thread);
+    }
+
+    public void btnOnClick(View view) {
+    }
+}
