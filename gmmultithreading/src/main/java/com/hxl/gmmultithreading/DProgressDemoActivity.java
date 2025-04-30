a0b74620e75144e4f5a9728c6283cb0c4a2706b3
huangxl <413612464@qq.com>
2025-04-30 09:04:37 +0800

    feat: 异步消息模似定时器实现进度条

--- gmmultithreading/src/main/java/com/hxl/gmmultithreading/DProgressDemoActivity.java
+++ gmmultithreading/src/main/java/com/hxl/gmmultithreading/DProgressDemoActivity.java
@@ -1,14 +1,19 @@
 package com.hxl.gmmultithreading;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.view.View;
 import android.widget.ProgressBar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DProgressDemoActivity extends AppCompatActivity {
 
     private ProgressBar pb_a;
+    private int progress = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +24,22 @@ public class DProgressDemoActivity extends AppCompatActivity {
     }
 
     public void btnOnClickA(View view) {
+        pb_a.setVisibility(View.VISIBLE);
+        handler.sendEmptyMessageDelayed(1, 1000);
     }
+
+    private final Handler handler = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            switch (msg.what) {
+                case 1:
+                    if (progress < 100) {
+                        progress += 10;
+                        pb_a.setProgress(progress);
+                        handler.sendEmptyMessageDelayed(1, 1000);
+                    }
+                    break;
+            }
+        }
+    };
 }
