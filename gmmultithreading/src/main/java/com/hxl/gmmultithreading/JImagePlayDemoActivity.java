3f74a6e740f26be197f8779698d2544ef74ad79a
huangxl <413612464@qq.com>
2025-05-08 19:53:42 +0800

    feat: 图片轮播器案例-UI

--- gmmultithreading/src/main/java/com/hxl/gmmultithreading/JImagePlayDemoActivity.java
+++ gmmultithreading/src/main/java/com/hxl/gmmultithreading/JImagePlayDemoActivity.java
@@ -3,12 +3,17 @@ package com.hxl.gmmultithreading;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 public class JImagePlayDemoActivity extends AppCompatActivity {
+
+    private ViewPager vp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jimage_play_demo);
+
+        vp = findViewById(R.id.vp);
     }
 }
