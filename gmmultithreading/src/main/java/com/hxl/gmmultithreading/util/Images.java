4607f4cb40615ccf7f72cf2d5473f56ef7a5d36f
huangxl <413612464@qq.com>
2025-05-08 19:54:04 +0800

    feat: 图片轮播器案例-图片集

---
+++ gmmultithreading/src/main/java/com/hxl/gmmultithreading/util/Images.java
@@ -0,0 +1,16 @@
+package com.hxl.gmmultithreading.util;
+
+import com.hxl.gmmultithreading.R;
+
+/**
+ * 轮播器测试图片集
+ */
+public class Images {
+    public static int[] imageArray = new int[]{
+            R.drawable.boy,
+            R.drawable.girl,
+            R.drawable.green,
+            R.drawable.grey,
+            R.drawable.orange
+    };
+}
