a4ff722832cf6ef71ef3fa7646e5b06867374cb1
huangxl <413612464@qq.com>
2025-05-08 19:52:54 +0800

    init: 图片轮播器案例

---
+++ gmmultithreading/src/main/java/com/hxl/gmmultithreading/JImagePlayDemoActivity.java
@@ -0,0 +1,14 @@
+package com.hxl.gmmultithreading;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class JImagePlayDemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jimage_play_demo);
+    }
+}
