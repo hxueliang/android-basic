d051619aaae0074f85b9df29b082a13198ea3fa5
huangxl <413612464@qq.com>
2025-05-14 12:58:44 +0800

    init: 初始化ContentResolver的Activity

--- b4contentresolver/src/main/java/com/hxl/b4contentresolver/AMainActivity.java
+++ b4contentresolver/src/main/java/com/hxl/b4contentresolver/AMainActivity.java
@@ -1,12 +1,9 @@
 package com.hxl.b4contentresolver;
 
 import android.os.Bundle;
+import android.view.View;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class AMainActivity extends AppCompatActivity {
 
@@ -14,5 +11,37 @@ public class AMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_amain);
+    }
+
+    /**
+     * 通过ContentResolver调用ContentProvider插入一条记录
+     *
+     * @param v
+     */
+    public void onInsert(View v) {
+    }
+
+    /**
+     * 通过ContentResolver调用ContentProvider删除一条记录
+     *
+     * @param v
+     */
+    public void onDelete(View v) {
+    }
+
+    /**
+     * 通过ContentResolver调用ContentProvider更新一条记录
+     *
+     * @param v
+     */
+    public void onUpdate(View v) {
+    }
+
+    /**
+     * 通过ContentResolver调用ContentProvider查询所有记录
+     *
+     * @param v
+     */
+    public void onQuery(View v) {
     }
 }
