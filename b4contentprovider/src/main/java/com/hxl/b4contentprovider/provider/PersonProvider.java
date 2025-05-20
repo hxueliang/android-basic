aed0c0969fd7d71561d6be6de1a011d6754e975a
huangxl <413612464@qq.com>
2025-05-20 18:58:59 +0800

    feat: ContentProvider-编辑PersonProvider类的query方法

--- b4contentprovider/src/main/java/com/hxl/b4contentprovider/provider/PersonProvider.java
+++ b4contentprovider/src/main/java/com/hxl/b4contentprovider/provider/PersonProvider.java
@@ -1,13 +1,18 @@
 package com.hxl.b4contentprovider.provider;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+
+import com.hxl.b4contentprovider.db.DBHelper;
 
 /**
  * 操作Person表的Provider类
@@ -16,6 +21,21 @@ import androidx.annotation.Nullable;
  * com.hxl.b4contentprovider.provider.PersonProvider
  */
 public class PersonProvider extends ContentProvider {
+    // 用来存放所有合法的Uri的容器
+    private static final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    /**
+     * 保存一些合法的Uri
+     * content://com.hxl.b4contentprovider.provider.personprovider/person   // 不根据id操作
+     * content://com.hxl.b4contentprovider.provider.personprovider/person/3 // 根据id操作
+     */
+    static {
+        matcher.addURI("com.hxl.b4contentprovider.provider.personprovider", "/person", 1);
+        matcher.addURI("com.hxl.b4contentprovider.provider.personprovider", "/person/#", 2); // #：匹配任意数字
+    }
+
+    private DBHelper dBHelper;
+
     public PersonProvider() {
         // 在AndroidManifest.xml文件中，注册了该Provider，就会执行
         Log.d("x_log", "PersonProvider");
@@ -25,14 +45,39 @@ public class PersonProvider extends ContentProvider {
     public boolean onCreate() {
         // 在AndroidManifest.xml文件中，注册了该Provider，就会执行
         Log.d("x_log", "PersonProvider onCreate");
+        dBHelper = new DBHelper(getContext());
         return false;
     }
 
+    /**
+     * content://com.hxl.b4contentprovider.provider.personprovider/person   // 不根据id查询
+     * content://com.hxl.b4contentprovider.provider.personprovider/person/3 // 根据id查询
+     */
     @Nullable
     @Override
     public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
         Log.d("x_log", "PersonProvider query");
-        return null;
+
+        Cursor cursor = null;
+
+        // 得到链接对象
+        SQLiteDatabase database = dBHelper.getReadableDatabase();
+
+        // 匹配uri，返回code
+        int code = matcher.match(uri);
+        // 如果合法，进行查询
+        if (code == 1) {
+            // 不根据id查询
+            cursor = database.query("person", projection, selection, selectionArgs, null, null, null);
+        } else if (code == 2) {
+            // 根据id查询
+            final long id = ContentUris.parseId(uri);
+            cursor = database.query("person", projection, "_id=?", new String[]{id + ""}, null, null, null);
+        } else {
+            // 如果不合法，抛出异常
+            throw new RuntimeException("查询的uri不合法");
+        }
+        return cursor;
     }
 
     @Nullable
