445df2634295325c047ce02c178f4b1428589b44
huangxl <413612464@qq.com>
2025-06-04 12:39:21 +0800

    feat: ContentProvider-编辑ContentResolver的onQuery方法

--- b4contentresolver/src/main/java/com/hxl/b4contentresolver/AMainActivity.java
+++ b4contentresolver/src/main/java/com/hxl/b4contentresolver/AMainActivity.java
@@ -1,7 +1,11 @@
 package com.hxl.b4contentresolver;
 
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -43,5 +47,17 @@ public class AMainActivity extends AppCompatActivity {
      * @param v
      */
     public void onQuery(View v) {
+        // 得到ContentResolver对象
+        final ContentResolver resolver = getContentResolver();
+        // 调用其query，得到cursor
+        final Uri uri = Uri.parse("content://com.hxl.b4contentprovider.provider.personprovider/person/1");
+        final Cursor cursor = resolver.query(uri, null, null, null, null);
+        // 取出cursor中的数据，并显示
+        if (cursor != null && cursor.moveToNext()) {
+            int id = cursor.getInt(0);
+            String name = cursor.getString(1);
+            Toast.makeText(this, id + " : " + name, Toast.LENGTH_LONG).show();
+        }
+
     }
 }
