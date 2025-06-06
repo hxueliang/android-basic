46acbe904a1faa6024fc353a23a425fa3e93c12b
huangxl <413612464@qq.com>
2025-06-06 09:27:46 +0800

    feat: 找回密码-验证码登录UI和切换登录方式

--- cintermediatecontrols/src/main/java/com/hxl/cintermediatecontrols/PLoginMainActivity.java
+++ cintermediatecontrols/src/main/java/com/hxl/cintermediatecontrols/PLoginMainActivity.java
@@ -1,15 +1,45 @@
 package com.hxl.cintermediatecontrols;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.RadioGroup;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class PLoginMainActivity extends AppCompatActivity {
+public class PLoginMainActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener {
+
+    private LinearLayout ll_password;
+    private LinearLayout ll_verify_code;
+    private CheckBox cb_remember;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_plogin_main);
+
+        final RadioGroup rg_login = findViewById(R.id.rg_login);
+        ll_password = findViewById(R.id.ll_password);
+        ll_verify_code = findViewById(R.id.ll_verify_code);
+        cb_remember = findViewById(R.id.cb_remember);
+
+        rg_login.setOnCheckedChangeListener(this);
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        // 密码登录
+        if (checkedId == R.id.rb_password) {
+            ll_password.setVisibility(View.VISIBLE);
+            cb_remember.setVisibility(View.VISIBLE);
+            ll_verify_code.setVisibility(View.GONE);
+        }
+        // 验证码登录
+        else if (checkedId == R.id.rb_verify_code) {
+            ll_password.setVisibility(View.GONE);
+            cb_remember.setVisibility(View.GONE);
+            ll_verify_code.setVisibility(View.VISIBLE);
+        }
     }
 }
