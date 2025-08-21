2e74e76d615f9babef75cf8944f3abcc5975a3c9
huangxl <413612464@qq.com>
2025-08-21 12:36:23 +0800

    feat: 购物车案例-数据库读取商品信息，并渲染到页面

--- dlocaldatalasting/src/main/java/com/hxl/dlocaldatalasting/JShoppingDetailActivity.java
+++ dlocaldatalasting/src/main/java/com/hxl/dlocaldatalasting/JShoppingDetailActivity.java
@@ -1,10 +1,14 @@
 package com.hxl.dlocaldatalasting;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.hxl.dlocaldatalasting.database.ShoppingDBHelper;
+import com.hxl.dlocaldatalasting.enity.GoodsInfo;
 
 public class JShoppingDetailActivity extends AppCompatActivity {
 
@@ -13,6 +17,7 @@ public class JShoppingDetailActivity extends AppCompatActivity {
     private TextView tv_goods_price;
     private TextView tv_goods_desc;
     private ImageView iv_goods_pic;
+    private ShoppingDBHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +31,26 @@ public class JShoppingDetailActivity extends AppCompatActivity {
         iv_goods_pic = findViewById(R.id.iv_goods_pic);
 
         tv_count.setText(String.valueOf(IMyApplication.getInstance().goodsCount));
+
+        dbHelper = ShoppingDBHelper.getInstance(this);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        showDetail();
+    }
+
+    private void showDetail() {
+        // 获取上一个页面传来的商品编号
+        int goodsId = getIntent().getIntExtra("goods_id", 0);
+        if (goodsId > 0) {
+            // 根据商品编号查询商品数据库中的商品记录
+            GoodsInfo info = dbHelper.queryGoodsInfoById(goodsId);
+            tv_title.setText(info.name);
+            tv_goods_desc.setText(info.description);
+            tv_goods_price.setText(String.valueOf((int) info.price));
+            iv_goods_pic.setImageURI(Uri.parse(info.picPath));
+        }
     }
 }
